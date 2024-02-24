6cccf89f0df6869ea810163c951f6e4f83a8c1f6
Slmii <selami.c@sbytes-it.com>
2024-02-24 19:29:43 +0100

    Add prayer time routes

---
+++ src/lib/routes/prayer-time.routes.ts
@@ -0,0 +1,50 @@
+import express, { NextFunction, Request, Response } from 'express';
+import { validate } from '../middlewares';
+import { memberSchema } from '../schemas';
+import { Member } from '../types';
+import { email } from '../services';
+
+const prayerTimeRoutes = express.Router();
+
+export interface PrayerTimeResponse {
+	Aksam: string;
+	AyinSekliURL: string;
+	GreenwichOrtalamaZamani: string;
+	Gunes: string;
+	GunesBatis: string;
+	GunesDogus: string;
+	HicriTarihKisa: string;
+	HicriTarihKisaIso8601: string;
+	HicriTarihUzun: string;
+	HicriTarihUzunIso8601: string;
+	Ikindi: string;
+	Imsak: string;
+	KibleSaati: string;
+	MiladiTarihKisa: string;
+	MiladiTarihKisaIso8601: string;
+	MiladiTarihUzun: string;
+	MiladiTarihUzunIso8601: string;
+	Ogle: string;
+	Yatsi: string;
+}
+
+prayerTimeRoutes.get('/today', async (_req: Request<any, any, Member>, res: Response, _next: NextFunction) => {
+	const response = await fetch('https://ezanvakti.herokuapp.com/vakitler/13880');
+	const data = (await response.json()) as PrayerTimeResponse[];
+
+	const now = new Date();
+
+	const prayerTime = data?.find(prayerTime => {
+		const prayerDate = new Date(prayerTime.MiladiTarihUzunIso8601.split('T')[0]);
+
+		return (
+			prayerDate.getDate() === now.getDate() &&
+			prayerDate.getMonth() === now.getMonth() &&
+			prayerDate.getFullYear() === now.getFullYear()
+		);
+	});
+
+	res.status(200).json({ prayerTime });
+});
+
+export { prayerTimeRoutes };
