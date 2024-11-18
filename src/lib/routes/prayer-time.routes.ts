fdad9321fb9bd4052865eeae06239ce1d07339cb
Slmii <selami.c@sbytes-it.com>
2024-11-18 22:58:24 +0100

    Add text representation for current prayer times in the current prayer time endpoint

--- src/lib/routes/prayer-time.routes.ts
+++ src/lib/routes/prayer-time.routes.ts
@@ -64,6 +64,8 @@ prayerTimeRoutes.get('/current', async (_req: Request<any, any, Member>, res: Re
 	const prayerTimeTomorrow = data[prayerTimeIndex + 1];
 
 	let time = '';
+	let text = '';
+
 	// Get current timestamp in HH:MM format
 	const currentTime = `${now.getHours()}:${now.getMinutes()}`;
 
@@ -71,26 +73,32 @@ prayerTimeRoutes.get('/current', async (_req: Request<any, any, Member>, res: Re
 
 	if (currentTime > prayerTime.Imsak && currentTime < prayerTime.Gunes) {
 		time = prayerTime.Gunes;
+		text = 'Güneş';
 	}
 	// Is Ogle
 	else if (currentTime > prayerTime.Gunes && currentTime < prayerTime.Ogle) {
 		time = prayerTime.Ogle;
+		text = 'Öğle';
 	}
 	// Is Ikindi
 	else if (currentTime > prayerTime.Ogle && currentTime < prayerTime.Ikindi) {
 		time = prayerTime.Ikindi;
+		text = 'İkindi';
 	}
 	// Is Aksam
 	else if (currentTime > prayerTime.Ikindi && currentTime < prayerTime.Aksam) {
 		time = prayerTime.Aksam;
+		text = 'Akşam';
 	}
 	// Is Yatsi
 	else if (currentTime > prayerTime.Aksam && currentTime < prayerTime.Yatsi) {
 		time = prayerTime.Yatsi;
+		text = 'Yatsı';
 	}
 	// Is Imsak of tomorrow
 	else {
 		time = prayerTimeTomorrow.Imsak;
+		text = 'İmsak';
 	}
 
 	res.status(200).json({ time });
