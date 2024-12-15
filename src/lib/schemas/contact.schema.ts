dc5efeceff8092fc1e01ddf15313a18ae4e12dcb
Slmii <selami.c@sbytes-it.com>
2024-12-15 01:49:46 +0100

    remove body

--- src/lib/schemas/contact.schema.ts
+++ src/lib/schemas/contact.schema.ts
@@ -1,12 +1,10 @@
 import { z } from 'zod';
 
 export const contactSchema = z.object({
-	body: z.object({
-		name: z.string({ required_error: 'Provide a valid name' }),
-		email: z.string().email('Provide a valid email address'),
-		phone: z.string({ required_error: 'Provide a valid phone number' }),
-		message: z.string({ required_error: 'Provide a message' })
-	})
+	name: z.string({ required_error: 'Provide a valid name' }),
+	email: z.string().email('Provide a valid email address'),
+	phone: z.string({ required_error: 'Provide a valid phone number' }),
+	message: z.string({ required_error: 'Provide a message' })
 });
 
 export type ContactSchema = z.infer<typeof contactSchema>;
