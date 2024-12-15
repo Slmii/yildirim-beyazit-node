4dfe29b997601f51537b3ca1d36cce38e5f6813d
Slmii <selami.c@sbytes-it.com>
2024-12-15 01:43:13 +0100

    Refactor contact and member schemas to remove unnecessary nested body objects for improved clarity

--- src/lib/schemas/member.schema.ts
+++ src/lib/schemas/member.schema.ts
@@ -1,17 +1,15 @@
 import { z } from 'zod';
 
 export const memberSchema = z.object({
-	body: z.object({
-		name: z.string({ required_error: 'Provide a valid name' }),
-		birthday: z.string({ required_error: 'Provide a valid birthday' }),
-		address: z.string({ required_error: 'Provide a valid address' }),
-		zip: z.string({ required_error: 'Provide a valid zip code' }),
-		phone: z.string({ required_error: 'Provide a valid phone number' }),
-		city: z.string({ required_error: 'Provide a valid city' }),
-		email: z.string().email('Provide a valid email address'),
-		bank: z.string({ required_error: 'Provide a valid bank' }),
-		amount: z.number().min(10, 'Minimum amount is 10')
-	})
+	name: z.string({ required_error: 'Provide a valid name' }),
+	birthday: z.string({ required_error: 'Provide a valid birthday' }),
+	address: z.string({ required_error: 'Provide a valid address' }),
+	zip: z.string({ required_error: 'Provide a valid zip code' }),
+	phone: z.string({ required_error: 'Provide a valid phone number' }),
+	city: z.string({ required_error: 'Provide a valid city' }),
+	email: z.string().email('Provide a valid email address'),
+	bank: z.string({ required_error: 'Provide a valid bank' }),
+	amount: z.number().min(10, 'Minimum amount is 10')
 });
 
 export type MemberSchema = z.infer<typeof memberSchema>;
