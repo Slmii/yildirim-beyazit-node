db9f3cc8050d8088b94a51813a02f1de877d7034
Slmii <selami.c@sbytes-it.com>
2024-12-15 01:45:23 +0100

    Refactor contact and member schemas to encapsulate fields within a body object for improved structure

--- src/lib/schemas/contact.schema.ts
+++ src/lib/schemas/contact.schema.ts
@@ -1,10 +1,12 @@
 import { z } from 'zod';
 
 export const contactSchema = z.object({
-	name: z.string({ required_error: 'Provide a valid name' }),
-	email: z.string().email('Provide a valid email address'),
-	phone: z.string({ required_error: 'Provide a valid phone number' }),
-	message: z.string({ required_error: 'Provide a message' })
+	body: z.object({
+		name: z.string({ required_error: 'Provide a valid name' }),
+		email: z.string().email('Provide a valid email address'),
+		phone: z.string({ required_error: 'Provide a valid phone number' }),
+		message: z.string({ required_error: 'Provide a message' })
+	})
 });
 
 export type ContactSchema = z.infer<typeof contactSchema>;
--- src/lib/schemas/member.schema.ts
+++ src/lib/schemas/member.schema.ts
@@ -1,15 +1,17 @@
 import { z } from 'zod';
 
 export const memberSchema = z.object({
-	name: z.string({ required_error: 'Provide a valid name' }),
-	birthday: z.string({ required_error: 'Provide a valid birthday' }),
-	address: z.string({ required_error: 'Provide a valid address' }),
-	zip: z.string({ required_error: 'Provide a valid zip code' }),
-	phone: z.string({ required_error: 'Provide a valid phone number' }),
-	city: z.string({ required_error: 'Provide a valid city' }),
-	email: z.string().email('Provide a valid email address'),
-	bank: z.string({ required_error: 'Provide a valid bank' }),
-	amount: z.number().min(10, 'Minimum amount is 10')
+	body: z.object({
+		name: z.string({ required_error: 'Provide a valid name' }),
+		birthday: z.string({ required_error: 'Provide a valid birthday' }),
+		address: z.string({ required_error: 'Provide a valid address' }),
+		zip: z.string({ required_error: 'Provide a valid zip code' }),
+		phone: z.string({ required_error: 'Provide a valid phone number' }),
+		city: z.string({ required_error: 'Provide a valid city' }),
+		email: z.string().email('Provide a valid email address'),
+		bank: z.string({ required_error: 'Provide a valid bank' }),
+		amount: z.number().min(10, 'Minimum amount is 10')
+	})
 });
 
 export type MemberSchema = z.infer<typeof memberSchema>;
